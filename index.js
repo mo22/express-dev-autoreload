82cb0210ea6cc9f243470521b942da5c586fa3f4
Moritz Moeller <mm@mxs.de>
2016-10-19 17:22:36 +0200

    ..

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
 const watch = require('node-watch');
 
-function expressFilterResponse(modifyCallback)
+function expressFilterResponse(checkCallback, modifyCallback)
 {
     return function expressFilterResponse(req, res, next) {
         var _end = res.end;
         var _write = res.write;
         var buffers = [];
-        res.write = function write(chunk, encoding) {
+        var addBuffer = (chunk, encoding) => {
+            if (chunk === undefined) return;
             if (typeof chunk === 'string') {
                 chunk = new Buffer(chunk, encoding);
             }
             buffers.push(chunk);
         };
+        res.write = function write(chunk, encoding) {
+            if (!res.headersSent) {
+                var hook = checkCallback(req, res);
+                if (!hook) {
+                    res.end = _end;
+                    res.write = _write;
+                    res.write(chunk, encoding);
+                } else {
+                    addBuffer(chunk, encoding);
+                }
+            } else {
+                addBuffer(chunk, encoding);
+            }
+        };
         res.end = function end(chunk, encoding) {
-            if (chunk) {
-                if (typeof chunk === 'string') {
-                    chunk = new Buffer(chunk, encoding);
-                }
-                buffers.push(chunk);
-            }
-            if (buffers.length == 0) {
-                _end.call(this);
-                return;
-            }
+            addBuffer(chunk, encoding);
             var buffer = Buffer.concat(buffers);
-            modifyCallback(req, res, buffer, (result) => {
-                if (typeof res.statusCode == 'string'){
-                    res.statusCode = res.statusCode.split(' ')[0] | 0;
+            Promise.resolve(modifyCallback(req, res, buffer)).then((result) => {
+                if (res.getHeader('Content-Length')) {
+                    res.setHeader('Content-Length', String(result.length));
                 }
-                res.setHeader('Content-Length', String(result.length));
-                _write.call(this, result);
-                _end.call(this);
+                res.end = _end;
+                res.write = _write;
+                res.write(result);
+                res.end();
+            }).catch((e) => {
+                // handle?
+                next(e);
             });
         };
         next();
@@ -60,14 +70,14 @@ module.exports = function expressDevAutoReload(options) {
             });
             return;
         }
-        expressFilterResponse((req, res, body, callback) => {
-            if (res.getHeader('Content-Type') && res.getHeader('Content-Type').startsWith('text/html')) {
-                body = body.toString();
-                var pos = body.search(new RegExp('</ *body *>', 'i'));
-                if (pos == -1) pos = body.length;
-                body = body.slice(0, pos) + '<script src="/__dev_autoreload__/livereload.js"></script>\n' + body.slice(pos);
-            }
-            callback(body);
+        expressFilterResponse((req, res) => {
+            return res.getHeader('Content-Type') && res.getHeader('Content-Type').startsWith('text/html');
+        }, (req, res, body) => {
+            body = body.toString();
+            var pos = body.search(new RegExp('</ *body *>', 'i'));
+            if (pos == -1) pos = body.length;
+            body = body.slice(0, pos) + '<script src="/__dev_autoreload__/livereload.js"></script>\n' + body.slice(pos);
+            return body;
         })(req, res, next);
     }
 }
